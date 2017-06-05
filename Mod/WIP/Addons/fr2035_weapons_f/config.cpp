592d4ea9e2fd8488044fde88be2ca323b3fc8bc2
Hector Corral <hectorcorral78@gmail.com>
2017-06-05 23:20:05 +0200

    Created Weapons empty addon

---
+++ Mod/WIP/Addons/fr2035_weapons_f/config.cpp
@@ -0,0 +1,19 @@
+class CfgPatches
+{
+	class FR2035_Characters_F
+	{
+		author="$STR_FR2035_Author";
+		name="French Army 2035 - Weapons and Accessories";
+		url="http://modflavors.com";
+		requiredAddons[]=
+		{
+			"FR2035_Data_F",
+			"A3_Weapons_F"
+		};
+		requiredVersion=1.66;
+		units[]={
+		};
+		weapons[]={
+		};
+	};
+};
