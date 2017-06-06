d6fdf6b941b25982b9f07273dadfb07d8dbc9d56
Hector Corral <hectorcorral78@gmail.com>
2017-06-06 17:43:41 +0200

    Added Assault Backpack and updated soundtrack

--- Mod/WIP/Addons/fr2035_weapons_f/config.cpp
+++ Mod/WIP/Addons/fr2035_weapons_f/config.cpp
@@ -1,6 +1,6 @@
 class CfgPatches
 {
-	class FR2035_Characters_F
+	class FR2035_Weapons_F
 	{
 		author="$STR_FR2035_Author";
 		name="French Army 2035 - Weapons and Accessories";
@@ -12,8 +12,24 @@ class CfgPatches
 		};
 		requiredVersion=1.66;
 		units[]={
+			"B_FR2035_AssaultPack_ce"
 		};
-		weapons[]={
+		weapons[]={};
+	};
+};
+class CfgVehicles
+{
+	class B_AssaultPack_Base;
+	class B_FR2035_AssaultPack_ce: B_AssaultPack_Base
+	{
+		author="$STR_FR2035_Author";
+		_generalMacro="B_FR2035_AssaultPack_ce";
+		scope=2;
+		displayName="$STR_FR2035_AssaultPack_ce";
+		picture="\fr2035_weapons_f\data\ui\fr2035_icon_b_compact_ce_ca.paa";
+		hiddenSelectionsTextures[]=
+		{
+			"\fr2035_weapons_f\data\fr2035_backpack_compact_ce_co.paa"
 		};
 	};
 };
