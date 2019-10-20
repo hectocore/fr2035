b010a59413b30c862158ded4aa770673aa72b3b9
Hector <hectorcorral78@gmail.com>
2019-10-20 14:06:50 +0200

    Fixed addon config of legacy armor

--- Mod/WIP/Addons/fr2035legacy_armor_f/config.cpp
+++ Mod/WIP/Addons/fr2035legacy_armor_f/config.cpp
@@ -320,19 +320,3 @@ class CfgVehicles
 		};
 	};
 };
-
-enum {
-	destructengine = 2,
-	destructdefault = 6,
-	destructwreck = 7,
-	destructtree = 3,
-	destructtent = 4,
-	stabilizedinaxisx = 1,
-	stabilizedinaxesxyz = 4,
-	stabilizedinaxisy = 2,
-	stabilizedinaxesboth = 3,
-	destructno = 0,
-	stabilizedinaxesnone = 0,
-	destructman = 5,
-	destructbuilding = 1
-}
