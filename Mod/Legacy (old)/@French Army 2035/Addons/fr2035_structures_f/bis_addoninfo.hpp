90acbad6df02642e41c068c7fceba93dc6433c9b
Hector <hectorcorral78@gmail.com>
2020-11-14 20:37:28 +0100

    chore(legacy): move legacy mod to conception folder

--- Mod/Legacy (old)/@French Army 2035/Addons/fr2035_structures_f/bis_addoninfo.hpp
+++
@@ -1,5 +0,0 @@
-class BIS_AddonInfo
-{
-	author="76561198057082213";
-	timepacked="1455906709";
-};
--- Mod/Legacy (old)/@French Army 2035/meta.cpp
+++
@@ -1,4 +0,0 @@
-protocol = 1;
-publishedid = 621431572;
-name = "French Army 2035 v0.4.8";
-timestamp = 5247601228767235070;
